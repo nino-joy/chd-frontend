a1189066f83f5976cc34ad6c0e7e9288d137778e
Nino Joy <ninojoy1234@gmail.com>
2022-03-21 11:17:12 +0530

    fixed desktopView - LandingScreen

--- pages/LandingScreen.js
+++ pages/LandingScreen.js
@@ -6,17 +6,17 @@ import Image from "next/image";
 import landingIMG from "../assets/images/landing.jpeg";
 
 const navigation = [
-	{ name: "Apple", href: "#" },
-	{ name: "Orange", href: "#" },
+	{ name: "About the Devs", href: "#" },
+	{ name: "Learn More", href: "#" },
 ];
 
 export default function LandingScreen() {
 	return (
-		<div className="h-full bg-white overflow-hidden">
+		<div className="h-screen bg-white overflow-hidden">
 			<div className="max-w-7xl mx-auto">
 				<div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
 					<svg
-						className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+						className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2"
 						fill="currentColor"
 						viewBox="0 0 100 100"
 						preserveAspectRatio="none"
@@ -26,7 +26,7 @@ export default function LandingScreen() {
 					</svg>
 
 					<Popover>
-						<div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+						<div className="sm:relative lg:fixed pt-6 px-4 sm:px-6 lg:px-8">
 							<nav
 								className="relative flex items-center justify-between sm:h-10 lg:justify-start"
 								aria-label="Global"
@@ -107,39 +107,33 @@ export default function LandingScreen() {
 						</Transition>
 					</Popover>
 
-					<main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-						<div className="sm:text-center lg:text-left">
-							<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-								<span className="block xl:inline">Optimal DNN Based</span>{" "}
-								<span className="block text-indigo-600 xl:inline">
-									Coronary Heart Disease Prediction
-								</span>
-							</h1>
-							<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
-							</p>
-							<div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-								<div className="rounded-md shadow">
-									<a
-										href="#"
-										className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-									>
-										Get started
-									</a>
+					<div className=" lg:flex lg:h-screen pt-5 md:pt-10 lg:pt-0 px-10">
+						<div className="flex justify-center items-center">
+							<div className="sm:text-center lg:text-left lg:mr-8">
+								<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+									<span className="block xl:inline">Optimal DNN Based</span>{" "}
+									<span className="block text-indigo-600 xl:inline">
+										Coronary Heart Disease Prediction
+									</span>
+								</h1>
+								<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+									qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+									occaecat fugiat aliqua.
+								</p>
+								<div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+									<div className="rounded-md shadow">
+										<a
+											href="#"
+											className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+										>
+											Get started
+										</a>
+									</div>
 								</div>
-								{/* <div className="mt-3 sm:mt-0 sm:ml-3">
-									<a
-										href="#"
-										className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-									>
-										Live demo
-									</a>
-								</div> */}
 							</div>
 						</div>
-					</main>
+					</div>
 				</div>
 			</div>
 			<div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
